d2f65a07a2ac1e048a6d146a78000b7fbb28c40a
David Llop <931684+sembrestels@users.noreply.github.com>
2022-05-31 19:14:49 +0200

    Move voting app files to its own directory

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { AragonProvider } from './hooks/shared/useAppState'
-import App from './App'
+import { AragonProvider } from './voting/hooks/shared/useAppState'
+import App from './voting/App'
 
 ReactDOM.render(
   <AragonProvider>
