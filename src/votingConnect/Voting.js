b148fc72257f54930ca0ccede4821579c780f18c
Rodrigo Perez <rodrigopi1989@gmail.com>
2022-07-06 15:40:26 -0300

    pr comments

--- src/votingConnect/Voting.js
+++ src/votingConnect/Voting.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useOrganizationState } from '../providers/OrganizatioProvider'
+import { useOrganizationState } from '../providers/OrganizationProvider'
 
 // This folder will be called just voting as the normal one after the migration to use connect is finished
 function Voting() {
