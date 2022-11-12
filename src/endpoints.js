2d2b7719877b3d05290d296965749f7acd9c42a6
PJColombo <paulo.colombo@pm.me>
2022-11-12 17:40:00 +0100

    chore: set blossom's ipfs gateway as default

--- src/endpoints.js
+++ src/endpoints.js
@@ -3,6 +3,6 @@ import { isLocalOrUnknownNetwork } from './utils/web3-utils'
 export const IPFS_ENDPOINT = {
   read: isLocalOrUnknownNetwork()
     ? 'http://127.0.0.1:8080/ipfs'
-    : 'https://ipfs.io/ipfs/',
+    : 'https://ipfs.blossom.software/ipfs/',
   upload: 'https://ipfs.infura.io:5001/api/v0/add',
 }
