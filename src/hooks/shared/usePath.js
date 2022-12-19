f2748637a8bee7ed62129fdc0de003e302103a21
PJColombo <paulo.colombo@pm.me>
2022-12-19 13:59:21 -0300

    feat(usePath): expose home navigation relative to the current app

--- src/hooks/shared/usePath.js
+++ src/hooks/shared/usePath.js
@@ -1,6 +1,30 @@
+import { useCallback } from 'react'
 import { useLocation, useHistory } from 'react-router-dom'
+
 export default function usePath() {
   const location = useLocation()
   const history = useHistory()
-  return [location.pathname, path => history.push(path)]
+  const path = location.pathname
+
+  const navigate = useCallback(
+    path => {
+      history.push(path)
+    },
+    [history]
+  )
+
+  const navigateToAppHome = useCallback(() => {
+    if (path.length < 3) {
+      return
+    }
+
+    navigate(
+      `/${path
+        .split('/')
+        .slice(1, 3)
+        .join('/')}`
+    )
+  }, [path, navigate])
+
+  return [path, navigate, navigateToAppHome]
 }
