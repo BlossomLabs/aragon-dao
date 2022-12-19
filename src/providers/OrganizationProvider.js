acf373403511f2f9d5b082f05b17af7527f377ec
PJColombo <paulo.colombo@pm.me>
2022-12-19 13:41:06 -0300

    feat(OrganizationProvider): connect to multiple apps of the same instance

--- src/providers/OrganizationProvider.js
+++ src/providers/OrganizationProvider.js
@@ -1,15 +1,36 @@
-import React, { useContext } from 'react'
+import React, { useContext, useMemo } from 'react'
 import { useConnect } from '@1hive/connect-react'
 import connectVoting from '@rperez89/connect-tao-voting'
 import connectTokenWrapper from '@rperez89/connect-token-wrapper'
 import { connectANDelay } from '@blossom-labs/connect-an-delay'
+import { usePath } from '@/hooks/shared'
+import { addressesEqual } from '@/utils/web3-utils'
 
 const OrganizationContext = React.createContext()
 
+const connectToApps = (apps, appNames, connectAppFns) => () => {
+  if (!apps) {
+    return
+  }
+
+  console.log(`Creating connectors for apps`)
+
+  let connectFns = []
+
+  appNames.forEach((appName, i) => {
+    const filteredApps = apps.filter(app => app.name === appName)
+    connectFns.push(...filteredApps.map(app => connectAppFns[i](app)))
+  })
+
+  return Promise.all(connectFns)
+}
+
 function OrganizationProvider({ children }) {
+  const [location] = usePath()
   const [org, orgStatus] = useConnect()
   const [apps, appsStatus] = useConnect(org => org.apps())
   const [permissions, permissionsStatus] = useConnect(org => org.permissions())
+  console.log(apps)
   const [connectedDisputableApp, connectedDisputableAppStatus] = useConnect(
     org => {
       return connectVoting(org.onApp('blossom-tao-voting'))
@@ -23,9 +44,36 @@ function OrganizationProvider({ children }) {
   const [connectedANDelayApp, connectedANDelayAppStatus] = useConnect(org =>
     connectANDelay(org.onApp('delay'))
   )
+
+  const [connectedApps, connectedAppsStatus] = useConnect(
+    connectToApps(
+      apps,
+      ['an-delay', 'delay', 'blossom-tao-voting', 'blossom-token-wrapper'],
+      [connectANDelay, connectANDelay, connectVoting, connectTokenWrapper]
+    ),
+    [apps]
+  )
+  const currentConnectedApp = useMemo(() => {
+    if (!connectedApps) {
+      return
+    }
+
+    const [, , currentAppAddress] = location.split('/')
+    return connectedApps.find(connectedApp =>
+      addressesEqual(connectedApp['_app']?.address, currentAppAddress)
+    )
+  }, [connectedApps, location])
+
   const loading =
-    orgStatus.loading || appsStatus.loading || permissionsStatus.loading
-  const error = orgStatus.error || appsStatus.error || permissionsStatus.error
+    orgStatus.loading ||
+    appsStatus.loading ||
+    permissionsStatus.loading ||
+    connectedAppsStatus.loading
+  const error =
+    orgStatus.error ||
+    appsStatus.error ||
+    permissionsStatus.error ||
+    connectedAppsStatus.error
 
   return (
     <OrganizationContext.Provider
@@ -39,6 +87,8 @@ function OrganizationProvider({ children }) {
         connectedDisputableAppStatus,
         connectedTokenWrapperApp,
         connectedTokenWrapperAppStatus,
+        connectedApps,
+        currentConnectedApp,
         permissions,
         loading,
         error,
