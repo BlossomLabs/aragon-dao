b4bcf50c3c0cb0fb451deca9f6556a9d106c912a
PJColombo <paulo.colombo@pm.me>
2023-01-30 07:34:28 -0300

    chore: remove unused `useApi()`

--- src/apps/voting/app-logic.js
+++ src/apps/voting/app-logic.js
@@ -1,10 +1,7 @@
 import { useCallback, useMemo } from 'react'
-import { useApi, usePath } from '@/hooks/shared'
+import { usePath } from '@/hooks/shared'
 import { useAppState } from './providers/VotingProvider'
-import { EMPTY_CALLSCRIPT } from './evmscript-utils'
 import useVotes from './hooks/useVotes'
-import { noop } from './utils'
-import { VOTE_YEA } from './vote-types'
 
 const VOTE_ID_PATH_RE = /^\/vote\/([0-9]+)\/?$/
 const NO_VOTE_ID = '-1'
@@ -42,47 +39,6 @@ export function useSelectedVote(votes) {
   return [selectedVote, selectVote]
 }
 
-// Create a new vote
-export function useCreateVoteAction(onDone = noop) {
-  const api = useApi()
-  return useCallback(
-    question => {
-      if (api) {
-        // Don't care about response
-        api['newVote(bytes,string)'](EMPTY_CALLSCRIPT, question).toPromise()
-        onDone()
-      }
-    },
-    [api, onDone]
-  )
-}
-
-// Vote (the action) on a vote
-export function useVoteAction(onDone = noop) {
-  const api = useApi()
-  return useCallback(
-    (voteId, voteType, executesIfDecided = true) => {
-      // Don't care about response
-      api.vote(voteId, voteType === VOTE_YEA, executesIfDecided).toPromise()
-      onDone()
-    },
-    [api, onDone]
-  )
-}
-
-// Execute a vote
-export function useExecuteAction(onDone = noop) {
-  const api = useApi()
-  return useCallback(
-    voteId => {
-      // Don't care about response
-      api.executeVote(voteId).toPromise()
-      onDone()
-    },
-    [api, onDone]
-  )
-}
-
 // Handles the main logic of the app.
 export function useAppLogic() {
   const { isSyncing } = useAppState()
@@ -90,14 +46,7 @@ export function useAppLogic() {
   const [votes, executionTargets] = useVotes()
   const [selectedVote, selectVote] = useSelectedVote(votes)
 
-  const actions = {
-    createVote: useCreateVoteAction(),
-    vote: useVoteAction(),
-    execute: useExecuteAction(),
-  }
-
   return {
-    actions,
     executionTargets,
     isSyncing,
     selectVote,
