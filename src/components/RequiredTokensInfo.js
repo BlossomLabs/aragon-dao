5f5509f1467c06cd13f5acadb898cd5934e5cf3e
PJColombo <paulo.colombo@pm.me>
2023-02-17 10:14:30 +0100

    feat: add veto warning to required tokens info box

--- src/components/RequiredTokensInfo.js
+++ src/components/RequiredTokensInfo.js
@@ -37,13 +37,21 @@ export default function RequiredTokensError({ ...props }) {
           <strong>{feeToken.symbol}</strong> tokens will be locked in order to
           perform this action. You'll get them back once the delay period of{' '}
           <strong>{formattedExecutionDelay}</strong> is completed.
+          <strong>
+            If the proposal is to be vetoed, Guardians will temporarily pause it
+            to ensure the proper recovery of the funds go to your address.
+          </strong>
         </>
       ) : (
         <>
           You need to have <strong>{formattedRequiredFeeAmount}</strong>{' '}
           <strong>{feeToken.symbol}</strong> in order to perform this action.
           You'll get them back once the delay period of{' '}
-          <strong>{formattedExecutionDelay}</strong> is completed.
+          <strong>{formattedExecutionDelay}</strong> is completed.{' '}
+          <strong>
+            If the proposal is to be vetoed, Guardians will temporarily pause it
+            to ensure the proper recovery of the funds go to your address.
+          </strong>
         </>
       )}
     </Info>
